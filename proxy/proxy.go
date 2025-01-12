1fe9eebd713cc792f653c77f640898f883e17bc8
Saksham <agsaksham07@gmail.com>
2025-01-12 20:56:18 +0530

    added redis caching

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -6,75 +6,91 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/redis/go-redis/v9"
-	"github.com/sakshamagrawal07/cache-proxy-server/cache"
 	"github.com/sakshamagrawal07/cache-proxy-server/utils"
 )
 
 type ProxyObject struct {
 	Origin string
-	Cache  map[string]*cache.CacheObject
 }
 
+// NewProxy creates a new ProxyObject
 func NewProxy(origin string) *ProxyObject {
 	return &ProxyObject{
 		Origin: origin,
-		Cache:  make(map[string]*cache.CacheObject), // Initialize the cache map
 	}
 }
 
 func (p *ProxyObject) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
+	ctx := context.Background()
+
+	// Connect to Redis
 	db := redis.NewClient(&redis.Options{
-		Network: "tcp",
 		Addr:     "localhost:6379",
-		Password: "", // Add password if needed
+		Password: "",
 		DB:       0,
 	})
 
-	ctx := context.Background()
+	CACHE_KEY := r.Method + ":" + r.URL.String() + ":" + "body"
 
-	result, err := db.Get(ctx, "name").Result()
-	if err != nil {
-		http.Error(w, "Error connecting to Redis", http.StatusInternalServerError)
-		log.Printf("Error connecting to Redis: %v", err)
+	// Get the response body from Redis
+	body, err := db.Get(ctx, CACHE_KEY).Result()
+
+	if err == redis.Nil {
+
+		// Cache not present
+		//Contruct the origin URL
+		var originUrl string
+
+		// If the request is for the root path, then the origin URL is the origin itself
+		if r.URL.String() == "/" {
+			originUrl = p.Origin
+		} else {
+			originUrl = p.Origin + r.URL.String()
+		}
+
+		// Forward the request to the origin server
+		res, err := http.Get(originUrl)
+		if err != nil {
+			http.Error(w, "Error in forwarding request.", http.StatusInternalServerError)
+			return
+		}
+
+		defer res.Body.Close()
+
+		// Read the response body
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			http.Error(w, "Error forwarding request body.", http.StatusInternalServerError)
+			return
+		}
+
+		// Cache the response body
+		// Store the response body in Redis
+		bodyCacheError := db.Set(ctx, CACHE_KEY, body, 0).Err()
+		if bodyCacheError != nil {
+			fmt.Println("Error caching response body")
+			log.Fatal(bodyCacheError)
+		}
+
+		fmt.Println("Cache Miss")
+		fmt.Println("Cache Key", CACHE_KEY)
+		fmt.Println("Origin URL", originUrl)
+		fmt.Println("Response", res)
+		fmt.Println("Body", body)
+		fmt.Printf("Response type %T\n", res)
+		fmt.Printf("Body type %T\n", body)
+
+		// Respond to the client
+		utils.RespondWithHeaders(w, res.StatusCode, body, "MISS", CACHE_KEY)
+	} else if err != nil {
+		log.Fatal(err)
 		return
+	} else {
+		fmt.Println("Cache Hit")
+		fmt.Println("Body", body)
+		utils.RespondWithHeaders(w, http.StatusOK, []byte(body), "Hit", CACHE_KEY)
 	}
-
-	fmt.Println("Result",result)
-
-	CACHE_KEY := r.Method + ":" + r.URL.String()
-
-	// Cache present
-	if cache, ok := p.Cache[CACHE_KEY]; ok {
-		utils.RespondWithHeaders(w, *cache.Response, cache.ResponseBody, "Hit", CACHE_KEY)
-		return
-	}
-
-	originUrl := p.Origin + r.URL.String()
-	res, err := http.Get(originUrl)
-
-	if err != nil {
-		http.Error(w, "Error in forwarding request.", http.StatusInternalServerError)
-		return
-	}
-
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-
-	if err != nil {
-		http.Error(w, "Error forwarding request body.", http.StatusInternalServerError)
-		return
-	}
-
-	p.Cache[CACHE_KEY] = &cache.CacheObject{
-		Response:     res,
-		ResponseBody: body,
-		Created:      time.Now(),
-	}
-
-	utils.RespondWithHeaders(w, *res, body, "MISS", CACHE_KEY)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -2,11 +2,8 @@ package utils
 
 import "net/http"
 
-func RespondWithHeaders(w http.ResponseWriter, r http.Response, body []byte, cacheHeader string, cacheKey string) {
+func RespondWithHeaders(w http.ResponseWriter, statusCode int, body []byte, cacheHeader string, cacheKey string) {
 	w.Header().Set("X-Cache", cacheHeader)
-	w.WriteHeader(r.StatusCode)
-	for k, v := range r.Header {
-		w.Header()[k] = v
-	}
+	w.WriteHeader(statusCode)
 	w.Write(body)
 }
